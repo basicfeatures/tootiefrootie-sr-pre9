6e3d8ed4a3ef7b797a613be8ee54d72fb39ffb88
leastbad <hello@leastbad.com>
2022-07-11 13:15:41 -0400

    working!

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,15 +2,20 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
-
-import HelloController from "./hello_controller"
+import { application } from './application'
 import StimulusReflex from 'stimulus_reflex'
 import consumer from '../channels/consumer'
 import controller from '../controllers/application_controller'
 import CableReady from 'cable_ready'
-application.register("hello", HelloController)
+import controllers from './**/*_controller.js'
+
+controllers.forEach(controller => {
+  application.register(controller.name, controller.module.default)
+})
+
 application.consumer = consumer
+
 StimulusReflex.initialize(application, { controller, isolate: true })
 StimulusReflex.debug = process.env.RAILS_ENV === 'development'
+
 CableReady.initialize({ consumer })
